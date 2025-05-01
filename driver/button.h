8e1173941b8e162bb9514eea776c32f6cefba61b
Riccardo Damiani <riccardodamiani.28@gmail.com>
2025-05-01 12:53:39 +0200

    changed variable name

--- driver/button.h
+++ driver/button.h
@@ -9,7 +9,7 @@
 #include "driver/driver.h"
 
 struct CESP_ButtonConfigStruct{
-    uint8_t buttonID;
+    uint8_t deviceID;
     uint8_t gpio_pin; // GPIO pin for button
     bool normally_open; // normally opened or closed?
     uint32_t debounce_time_ms; // Debounce time in milliseconds
@@ -32,7 +32,7 @@ public:
 
 private:
 struct InternalDeviceInfo{
-    uint8_t buttonID;
+    uint8_t deviceID;
     uint8_t gpio_pin;
     uint8_t normally_open; // normally opened or closed?
     uint32_t debounce_time_ms; // Debounce time in milliseconds
